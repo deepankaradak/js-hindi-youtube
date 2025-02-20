b8ef4ca14a6759aa6906da11b46b993cbfb524dc
Deepankar Adak <85246434+deepankaradak@users.noreply.github.com>
2025-02-20 09:46:58 +0000

    conversion of data types operations

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,30 @@
+// let score = "33a"
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN     (Not a number)
+// true => 1; false => 0;
+
+
+let isLoggedIn = "Deepankar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "Deepankar" => true
+
+
+let someNumber = 11
+
+let numberString = String(someNumber);
+console.log(typeof numberString);
+
