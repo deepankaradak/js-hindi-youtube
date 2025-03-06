42797cd7a64317c10b97465b86d7d3554991758a
Deepankar Adak <85246434+deepankaradak@users.noreply.github.com>
2025-03-06 21:16:33 +0000

    strated with functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+function saymyname(){
+    console.log("D");
+console.log("E");
+console.log("E");
+console.log("P");
+console.log("A");
+console.log("N");
+console.log("K");
+console.log("A");
+console.log("R");
+}
+
+// saymyname();
+
+function addTwoNumbers(num1,num2){
+    console.log(num1 + num2);
+    
+}
+
+addTwoNumbers(5,3);
