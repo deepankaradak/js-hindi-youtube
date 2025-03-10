186b8280e73c2782685b3a686eb8bac2b369f0cb
Deepankar Adak <85246434+deepankaradak@users.noreply.github.com>
2025-03-10 18:14:18 +0000

    functions and nested scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,11 +8,56 @@ const b = 20;
 console.log("INNER:",a);
 }
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
-}
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// nested scope
+
+function one(){
+    const username = "Deepankar"
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    // two();
+}
+
+one();
+
+// ---------------------------------------------
+
+if(true){
+    const username = "deepankar";
+    if(username === "deepankar"){
+        const website = " youtube";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// -=================-----------------------intresting
+console.log(addone(4));
+function addone(num){
+    return num + 1;
+
+}
+
+// console.log(addone(5));
+
+// console.log(addtwo(1));
+
+const addtwo = function(num){
+    return num +2
+}
+
+// console.log(addtwo(1));
