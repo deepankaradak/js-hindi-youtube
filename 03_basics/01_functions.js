a3bd458d1fc2927a5f3da788e826f55ed0b1d970
Deepankar Adak <85246434+deepankaradak@users.noreply.github.com>
2025-03-10 04:18:14 +0000

    done with function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,9 +12,62 @@ console.log("R");
 
 // saymyname();
 
-function addTwoNumbers(num1,num2){
-    console.log(num1 + num2);
+// function addTwoNumbers(num1,num2){
+//     console.log(num1 + num2);
     
+// }
+
+// addTwoNumbers(5,3);
+
+// const result = addTwoNumbers(5,3);
+// console.log("Result :", result);
+// -----------------------------------------
+// function addTwoNumbers(num1,num2){
+//        let result = num1 + num2;
+//     //    console.log(result);
+//        return result;
+// }
+
+// const result = addTwoNumbers(9,3);
+// console.log("Result :", result);
+
+// -----------------------------------------
+// function addTwoNumbers(num1,num2){
+//        return num1+num2;
+// }
+
+// const result = addTwoNumbers(9,3);
+// console.log("Result :", result);
+
+// -----------------------------------------
+
+// function loginUserMessage(username){
+//     // if(username === undefined){
+//     //     console.log("Please enter username");
+//     //     return
+        
+//     // }
+//     if(!username){
+//         console.log("Please enter username");
+//         return
+        
+//     }
+//     return `${username} just logged in.`
+// }
+
+// console.log(loginUserMessage("Deepankar"));
+// console.log(loginUserMessage());
+
+// ----------------------------------------------
+function loginUserMessage(username='sam'){
+   
+    if(!username){
+        console.log("Please enter username");
+        return
+        
+    }
+    return `${username} just logged in.`
 }
 
-addTwoNumbers(5,3);
+console.log(loginUserMessage());
+console.log(loginUserMessage("Deepankar"));
