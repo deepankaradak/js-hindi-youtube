043e1d9f3daf87880cc618782485b420c71fc402
Deepankar Adak <85246434+deepankaradak@users.noreply.github.com>
2025-03-10 09:13:39 +0000

    function with rest and spread operator

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -69,5 +69,44 @@ function loginUserMessage(username='sam'){
     return `${username} just logged in.`
 }
 
-console.log(loginUserMessage());
-console.log(loginUserMessage("Deepankar"));
+// console.log(loginUserMessage());
+// console.log(loginUserMessage("Deepankar"));
+
+// ------------------------------------------------- rest operator and spreed operator
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// function calculateCartPrice(val1,val2,...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,400,500, 2000));
+
+// -------------------------------------------------------------
+
+const user = {
+    username: "Deepankar",
+    price: 199
+}
+
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user);
+// handleObject({username:"sam",price:58})
+
+// ------------------------------------------------------------------------
+
+const myNewArray = [200,400,100,600];
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([1,2,3,4]));
+// console.log()
